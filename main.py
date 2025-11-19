bcc1130203b991d717aee30735ddb6d5fea1c365
preetihallur <preetibhallur@gmail.com>
2025-11-19 20:55:35 +0530

    Adding new feature

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ def main():
 
     total = sum(scores)
     average = total / len(scores)
+    max_score = max(scores)
+    min_score = min(scores)
 
     print(f"Sum of scores: {total}")
     print(f"Average of scores: {average:.2f}")
+    print(f"Maximum score: {max_score}")
+    print(f"Minimum score: {min_score}")
 
 if __name__ == "__main__":
     main()
